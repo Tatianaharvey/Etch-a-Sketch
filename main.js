d0ea37842f8d5f043ac2adf77b42b3821d5a0fa0
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-12 17:26:58 -0700

    add color feature

--- main.js
+++ main.js
@@ -5,10 +5,25 @@ function makeGrid (rows, cols) {
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
+        cell.addEventListener('mouseover', changeGridColor)
         container.appendChild(cell).className = "gridItem";
-    };
-
-    
+    };  
 };
+
+function changeGridColor(e) {
+    let colors = ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CADDBF', 
+    '#9BF6FF', '#A0C4FF', '#A0C4FF', '#BDB2FF', '#FFC6FF'];
+    let randomColor = Math.floor(Math.random() * colors.length);
+    e.target.style.opacity = 1;
+    e.target.style.backgroundColor = colors[randomColor];
+
+}
+
 makeGrid(16, 16);
+
+const resetGrid = document.querySelector('.resetGrid') 
+
+resetGrid.addEventListener ('click', () => {
+    window.location.reload();
+})
 
