3c92ffacbe89b3a7c70dab2cbacf022420510e40
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-12 21:31:35 -0700

    fix bug

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const container = document.querySelector('.container');
 
 function makeGrid (rows, cols) {
+    removeAllChildNodes(container);
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
-        cell.addEventListener('mouseover', changeGridColor)
-        container.appendChild(cell).className = "gridItem";
+    let cell = document.createElement("div");
+    cell.addEventListener('mouseover', changeGridColor)
+    container.appendChild(cell).className = "gridItem";
     };  
 };
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 function changeGridColor(e) {
     let colors = ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CADDBF', 
@@ -19,11 +26,26 @@ function changeGridColor(e) {
 
 }
 
-makeGrid(16, 16);
-
 const resetGrid = document.querySelector('.resetGrid') 
 
 resetGrid.addEventListener ('click', () => {
     window.location.reload();
 })
 
+const gridSize = document.querySelector('.gridSize');
+
+gridSize.addEventListener("click", changeSize);
+
+function changeSize() {
+    let newSize = prompt("Type a number between 1 and 100 to change the grid size");
+    let num = parseInt(newSize);
+    if (num > 1 && num <= 100) {
+        makeGrid(num,num);
+    } else {
+        alert("Please enter a digit between 1 and 100.")
+        changeSize()
+    }
+}
+
+// Main
+makeGrid(16,16);
